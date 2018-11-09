074d08ea162717d154c03833690294a3ebf86373
robert.lee <robert0714@gmail.com>
2018-11-09 11:27:29 +0800

    adjust

--- Chapter15/chapter15.02-calendar/src/main/java/com/packtpub/springsecurity/configuration/JavaConfig.java
+++ Chapter15/chapter15.02-calendar/src/main/java/com/packtpub/springsecurity/configuration/JavaConfig.java
@@ -1,7 +1,7 @@
 package com.packtpub.springsecurity.configuration;
 
 import com.packtpub.springsecurity.repository.RememberMeTokenRepository;
-import com.packtpub.springsecurity.web.authentication.rememberme.JpaTokenRepositoryCleaner;
+//import com.packtpub.springsecurity.web.authentication.rememberme.JpaTokenRepositoryCleaner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,13 +19,13 @@ public class JavaConfig {
     @Autowired
     private RememberMeTokenRepository rememberMeTokenRepository;
 
-    @Scheduled(fixedRate = 600_000)
-    public void tokenRepositoryCleaner(){
-        Thread trct = new Thread(
-                new JpaTokenRepositoryCleaner(
-                        rememberMeTokenRepository,
-                        100_000L));
-        trct.start();
-    }
+//    @Scheduled(fixedRate = 600_000)
+//    public void tokenRepositoryCleaner(){
+//        Thread trct = new Thread(
+//                new JpaTokenRepositoryCleaner(
+//                        rememberMeTokenRepository,
+//                        100_000L));
+//        trct.start();
+//    }
 
 } // The End...
